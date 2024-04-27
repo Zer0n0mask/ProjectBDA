5f118fc6958f9bc0ac34e6c631bcc9de858b0edb
Daniel Vega Pardo <danielvegapardo@gmail.com>
2024-04-27 15:40:02 -0500

    Daniel-autoBackUp-Sat Apr 27 03:40:02 PM -05 2024

--- DesarrolloDetallado/DIMENSIONAL/ETL/02_insert_dim_department.sql
+++ DesarrolloDetallado/DIMENSIONAL/ETL/02_insert_dim_department.sql
@@ -2,8 +2,8 @@ DELETE FROM grad_dimensional.dim_department;
 
 INSERT INTO grad_dimensional.dim_department(
 id_department,
-name,
-faculty)
+department_name,
+faculty_name)
 SELECT idDepartment as id_department,
 department_name as name,
 f.faculty_name as faculty
---
+++ autoPush.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Define the path to the lock file
+LOCK_FILE="/tmp/auto_backup_togit.lock"
+
+# Check if the lock file exists
+if [ -e "$LOCK_FILE" ]; then
+    echo "Another instance of the script is already running. Exiting."
+    exit 1
+fi
+
+# Create the lock file
+touch "$LOCK_FILE"
+
+# Change directory to your project folder
+cd /home/MySQL/ProjectBDA
+
+# Display the SSH public key
+cat ~/.ssh/id_rsa.pub
+
+# Infinite loop for continuous execution
+while true
+do
+    # Add all changes
+    git add .
+
+    # Commit changes
+    git commit -m "Daniel-autoBackUp-$(date)"
+
+    # Push changes to GitHub
+    echo "------------Starting to push to Github------------"
+    git push git@github.com:JustLokust/<Projectname> master
+    echo "------------Finished pushing to Github------------"
+
+    # Wait for 300 seconds (5 minutes)
+    sleep 300
+done
+
+# Remove the lock file when the script exits
+rm "$LOCK_FILE"
+
